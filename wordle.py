b54334a7c702961deb12f5babb6395d3bc0a790e
Nicolas Tricot <nicolast@padok.fr>
2024-04-01 12:58:21 +0200

    Adding 'must_have' letters management

--- wordle.py
+++ wordle.py
@@ -5,11 +5,20 @@ def load_dictionary(file_path):
         return [word.strip().lower() for word in file.readlines()]
 
 def initialize_possible_letters():
-    return [list(string.ascii_lowercase) for _ in range(5)]
+    possible_letters = {
+        'word': [list(string.ascii_lowercase) for _ in range(5)],
+        'must_have': []
+    }
+    return possible_letters
 
 def log_possible_letters(possible_letters):
-    for i, letters in enumerate(possible_letters):
-        print(f"[Position {i}] => {letters}")
+    for key, value in possible_letters.items():
+        if key == 'word':
+            print(f"[{key}]")
+            for i, letters in enumerate(value):
+                print(f"  [Position {i}] => {letters}")
+        elif key == 'must_have':
+            print(f"[{key}] => {''.join(value)}")
 
 def update_possible_letters(possible_letters, input_string):
     excluded_letters = input_string[input_string.find('(')+1:input_string.find(')')]
@@ -17,13 +26,14 @@ def update_possible_letters(possible_letters, input_string):
     
     for letter in excluded_letters:
         for i in range(5):
-            possible_letters[i] = [l for l in possible_letters[i] if l != letter]
+            possible_letters['word'][i] = [l for l in possible_letters['word'][i] if l != letter]
 
     for i, char in enumerate(input_string):
         if char.islower():
-            possible_letters[i] = [letter for letter in possible_letters[i] if letter != char]
+            possible_letters['word'][i] = [letter for letter in possible_letters['word'][i] if letter != char]
+            possible_letters['must_have'].append(char)
         elif char.isupper():
-            possible_letters[i] = [char.lower()]
+            possible_letters['word'][i] = [char.lower()]
     log_possible_letters(possible_letters)
     return True
     
@@ -31,7 +41,7 @@ def update_possible_letters(possible_letters, input_string):
 def filter_words(words, possible_letters):
     filtered_words = []
     for word in words:
-        if all(word[i] in possible_letters[i] for i in range(5)):
+        if all(word[i] in possible_letters['word'][i] for i in range(5)) and all(letter in word for letter in possible_letters['must_have']):
             filtered_words.append(word)
     return filtered_words
 
